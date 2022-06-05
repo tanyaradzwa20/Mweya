6c874879c1302fa9ce5da41180c1136e7b6f1f1a
Tanya <tanya@uncommon.org>
2022-06-05 17:12:48 +0200

    last commits

--- main.js
+++ main.js
@@ -43,4 +43,35 @@ window.addEventListener("DOMContentLoaded", function () {
     xhr.send(data);
   }
   
-
+  $(document).ready(function(){
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: '60px',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                // responsive options
+                slidesToShow: 1,
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                // responsive options
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                  // responsive options
+                  slidesToShow: 3,
+                }
+              },
+          ]
+    });
+  });
+      
